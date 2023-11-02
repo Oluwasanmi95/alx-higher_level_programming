b7db54b12365af1c697a072f7f73d33a88d2872b
Oluwasanmi <sanmioluwa@hotmail.com>
2023-11-02 11:09:56 -0700

    Infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    count = len(argv) - 1
+    total = 0
+    for i in range(count):
+        total += int(argv[i + 1])
+
+    print("{}".format(total))
